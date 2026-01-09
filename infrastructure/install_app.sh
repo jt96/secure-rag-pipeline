aa7fdc654655d2cbc57d00bf071c402fcc448afb
unknown <29517154+jt96@users.noreply.github.com>
2026-01-08 17:19:46 -0800

    Update install app script to install awscli v2

--- infrastructure/install_app.sh
+++ infrastructure/install_app.sh
@@ -3,7 +3,12 @@ set -e
 
 # --- System Updates & Docker Installation ---
 sudo apt-get update
-sudo apt-get install -y docker.io unzip awscli jq
+sudo apt-get install -y docker.io unzip jq
+
+echo "Installing AWS CLI v2..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 sudo systemctl start docker
 sudo systemctl enable docker

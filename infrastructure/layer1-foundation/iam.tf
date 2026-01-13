ca8af94d60555c2a62772f5785b9ea4f5234518c
unknown <29517154+jt96@users.noreply.github.com>
2026-01-12 17:53:04 -0800

    Add cloudwatch policy to ec2 role

--- infrastructure/layer1-foundation/iam.tf
+++ infrastructure/layer1-foundation/iam.tf
@@ -35,6 +35,11 @@ resource "aws_iam_role_policy_attachment" "ssm_attach" {
   policy_arn = aws_iam_policy.ssm_read_policy.arn
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "hybrid_rag_ec2_profile"
   role = aws_iam_role.ec2_role.name
